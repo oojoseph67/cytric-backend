9bd6afb36d19a1eb6abc520ea29d36b7398da0a9
Joseph <oojoseph67@gmail.com>
2025-02-18 01:25:50 +0100

    chore: swagger and compodoc added

--- src/nfts/dto/create-nft.dto.ts
+++ src/nfts/dto/create-nft.dto.ts
@@ -1,22 +1,49 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsNumber, IsString, IsUrl } from 'class-validator';
 
 export class CreateNftDto {
+  @ApiProperty({
+    description: 'The name of the NFT',
+    example: 'Cosmic Voyager #123',
+    type: 'string',
+  })
   @IsString()
   @IsNotEmpty()
   nftName: string;
 
+  @ApiProperty({
+    description: 'A detailed description of the NFT',
+    example:
+      'A unique digital artwork featuring a cosmic explorer traversing the universe',
+    type: 'string',
+  })
   @IsString()
   @IsNotEmpty()
   nftDescription: string;
 
+  @ApiProperty({
+    description: 'The URL pointing to the NFT image/media',
+    example: 'https://example.com/nft/cosmic-voyager-123.png',
+    type: 'string',
+  })
   @IsUrl()
   @IsNotEmpty()
   nftLogoUrl: string;
 
+  @ApiProperty({
+    description: 'The unique identifier for the NFT',
+    example: 123,
+    type: 'number',
+  })
   @IsNumber()
   @IsNotEmpty()
   nftId: number;
 
+  @ApiProperty({
+    description: 'The blockchain wallet address of the NFT owner',
+    example: '0x742d35Cc6634C0532925a3b844Bc454e4438f44e',
+    type: 'string',
+  })
   @IsString()
   @IsNotEmpty()
   userWallet: string;
--- src/nfts/nfts.controller.ts
+++ src/nfts/nfts.controller.ts
@@ -1,28 +1,83 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Param,
-  Query,
-} from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Query } from '@nestjs/common';
 import { NftsService } from './nfts.service';
 import { CreateNftDto } from './dto/create-nft.dto';
+import {
+  ApiBody,
+  ApiOperation,
+  ApiParam,
+  ApiResponse,
+  ApiTags,
+  ApiQuery,
+} from '@nestjs/swagger';
 
 @Controller('nfts')
+@ApiTags('NFTs')
 export class NftsController {
   constructor(private readonly nftsService: NftsService) {}
 
+  @ApiOperation({
+    summary: 'Create an NFT',
+    description: 'Creates a new NFT and associates it with a wallet address',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'NFT successfully created',
+    type: CreateNftDto,
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Bad Request - Invalid input data',
+  })
+  @ApiBody({
+    type: CreateNftDto,
+    description: 'NFT creation data',
+  })
   @Post()
   create(@Body() createNftDto: CreateNftDto) {
     return this.nftsService.create({ nftData: createNftDto });
   }
 
+  @ApiOperation({
+    summary: 'Find NFTs by wallet address',
+    description: 'Retrieves all NFTs associated with a specific wallet address',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'List of NFTs found',
+    type: [CreateNftDto],
+  })
+  @ApiQuery({
+    name: 'userWallet',
+    description: 'Wallet address to search NFTs for',
+    example: '0x742d35Cc6634C0532925a3b844Bc454e4438f44e',
+    required: true,
+    type: String,
+  })
   @Get('/findByWallet')
   findByWallet(@Query('userWallet') userWallet: string) {
     return this.nftsService.findByWallet(userWallet);
   }
 
+  @ApiOperation({
+    summary: 'Find NFT by ID',
+    description: 'Retrieves a specific NFT by its ID',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'NFT found',
+    type: CreateNftDto,
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'NFT not found',
+  })
+  @ApiParam({
+    name: 'id',
+    description: 'NFT ID to search for',
+    example: 123,
+    type: Number,
+    required: true,
+  })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.nftsService.findOne(+id);
